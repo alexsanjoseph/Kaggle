09a4d85b2eece5456bce412ab2c7ec8b1bf74dfd
alexsanjoseph <alexsanjoseph@gmail.com>
2015-11-29 16:03:47 +0530

    XGBoost fail

--- Rossman/rossman_exploratory.R
+++ Rossman/rossman_exploratory.R
@@ -4,6 +4,7 @@ library(zoo)
 library(forecast)
 library(ggplot2)
 library(dplyr)
+library(xgboost)
 
 # Get Data ----
 test <- fread("~/Learning/Data/Kaggle/rossman/test.csv")
@@ -14,14 +15,31 @@ samp_sub <- fread("~/Learning/Data/Kaggle/rossman/sample_submission.csv")
 
 # Massage Data ----
 train$Date = as.Date(train$Date)
-train_subset = sample_n(train, 10000)
+train_subset = train %>% select(-Customers) #%>% sample_n(10000) 
 single_store = filter(train, Store == 1)
 
 # Exploratory ----
-ggplot(single_store, aes(x = Date, y = Customers)) + geom_line()
+# ggplot(single_store, aes(x = Date, y = Customers)) + geom_line()
 
 # Prediction ----
-samp_sub$Sales = mean(train$Sales)
+# samp_sub$Sales = mean(train$Sales)
+
+xgb_data_train = model.matrix( ~ . - 1, data = train_subset %>% select(-Date, -Id)) 
+dtrain <- xgb.DMatrix(xgb_data_train[,-3], label = xgb_data_train[,3])
+xgb_model = xgboost(data = dtrain, nrounds = 100)
+
+test_subset = test %>% select(-Date, -Id) %>% mutate(Open = replace(Open, is.na(Open), 1))
+xgb_data_test = model.matrix( ~ . - 1, data = test_subset)
+nrow(xgb_data_test)
+
+dtest <- xgb.DMatrix(xgb_data_test)
+pred = predict(xgb_model, dtest)
+plot(pred)
+pred
+
+
 
 # Submission ----
+sub = data.frame(Id = test$Id, Sales = pred)
+head(samp_sub)
 write.csv(samp_sub, "~/Learning/Data/Kaggle/rossman/subs/sub_0.csv", row.names = F)
