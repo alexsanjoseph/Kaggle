219c9e06a9848c0dd688f6af9d8728be65e0d39f
alexsanjoseph <alexsanjoseph@gmail.com>
2015-12-31 23:48:47 +0530

    Multiple clusters using image thresholding

--- web-scrape/denoising_dirty_docs/image_thresholding.R
+++ web-scrape/denoising_dirty_docs/image_thresholding.R
@@ -1,7 +1,3 @@
-dirtyFolder = "../Data/Kaggle/denoising_dirty/train"
-cleanFolder = "../Data/Kaggle/denoising_dirty/train_cleaned"
-outFolder = "../Data/Kaggle/denoising_dirty/train_predicted"
-
 # libraries
 library(data.table)
 pacman::p_load(png, raster)
@@ -20,3 +16,26 @@ img2vec = function(img)
 
 # show a histogram
 hist(img2vec(img))
+
+# fit 3 clusters
+v = img2vec(img)
+km.mod = kmeans(v, 3)
+
+# allow for the random ordering of the clusters
+oc = order(km.mod$centers)
+# the lower threshold is the halfway point between the top of the lowest cluster and the bottom of the middle cluster
+loThresh = 0.5 * (max(v[km.mod$cluster == oc[1]]) + min(v[km.mod$cluster == oc[2]]))
+# the higher threshold is the halfway point between the top of the middle cluster and the bottom of the highest cluster
+hiThresh = 0.5 * (max(v[km.mod$cluster == oc[2]]) + min(v[km.mod$cluster == oc[3]]))
+
+# using lower threshold
+imgLo = img
+imgLo[imgLo <= loThresh] = 0 
+imgLo[imgLo > loThresh] = 1
+plot(raster(imgLo))
+
+# using upper threshold
+imgHi = img
+imgHi[imgHi <= hiThresh] = 0
+imgHi[imgHi > hiThresh] = 1
+plot(raster(imgHi))
