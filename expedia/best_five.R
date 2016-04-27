b384be4b0ff0cbe7add5e7a2220b3403d90dc6da
alexsanjoseph <alexsanjoseph@gmail.com>
2016-04-27 19:35:54 +0530

    [expedia] fully data leak abuse

--- expedia/best_five.R
+++ expedia/best_five.R
@@ -11,31 +11,31 @@ train_data = fread("../Data/kaggle/expedia/train.csv")
 
 ###### Best Five
 
-# head(sample_sub)
 # 
-# train_freq = train_data[ , list(length(site_name)), by = c('hotel_cluster')]
-# train_freq = train_freq %>% arrange(desc(V1))
-# head(train_freq)
+train_freq = train_data[ , list(length(site_name)), by = c('hotel_cluster')]
+train_freq = train_freq %>% arrange(desc(V1))
 # 
-# best_five = train_freq$hotel_cluster[1:5] %>% paste(collapse = " ")
+best_five = train_freq$hotel_cluster[1:5] %>% paste(collapse = " ")
 # 
-# head(train_data)
-# test_data$id %>% head
+# write.csv(data.frame(id = test_data$id, hotel_cluster = best_five),
+          # "../Data/kaggle/expedia/submissions/sub2.csv", row.names = F)
+
 
 ##### Data Leak
-train_data_subset = train_data[1:1000000,]
-setkeyv(train_data_subset, c('user_location_country', 'user_location_region', 'user_location_city', 'hotel_market', ))
+names(train_data)
+train_data_subset = train_data #[1:1000000,]
+setkeyv(train_data_subset, c('user_location_country', 'user_location_region', 'user_location_city', 'hotel_market', 'orig_destination_distance'))
 
 system.time({
   hotel_cluster_obs = train_data_subset[ , list(most_common = most_common(hotel_cluster)), by = key(train_data_subset)] 
 })
 
-A = train_data[user_id == 12]
-A
-A = train_data_subset[, key(train_data_subset), with = F]
-A1 = unique(A)
-                                                                  
-write.csv(data.frame(id = test_data$id, hotel_cluster = best_five),
-          "../Data/kaggle/expedia/submissions/sub1.csv", row.names = F)
+output = test_data %>% merge(hotel_cluster_obs, by = key(hotel_cluster_obs), all.x = T)
+head(output)                                                                  
+submission = output %>% select(id, hotel_cluster = most_common) %>% 
+  mutate(hotel_cluster = replace(hotel_cluster, is.na(hotel_cluster), best_five)) %>% 
+  arrange(id)
+
+write.csv(submission, "../Data/kaggle/expedia/submissions/sub3.csv", row.names = F)
 
                                                                   
