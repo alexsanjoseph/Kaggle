06d23c156f554e9b068dab38051531c6d4e6478a
alexsanjoseph <alexsanjoseph@gmail.com>
2016-04-28 09:59:36 +0530

    [expedia][sub5]using tar gz

--- expedia/best_five.R
+++ expedia/best_five.R
@@ -23,19 +23,43 @@ best_five = train_freq$hotel_cluster[1:5] %>% paste(collapse = " ")
 
 ##### Data Leak
 names(train_data)
+
 train_data_subset = train_data #[1:1000000,]
+
+# Using the full data leak
 setkeyv(train_data_subset, c('user_location_country', 'user_location_region', 'user_location_city', 'hotel_market', 'orig_destination_distance'))
 
 system.time({
   hotel_cluster_obs = train_data_subset[ , list(most_common = most_common(hotel_cluster)), by = key(train_data_subset)] 
 })
 
-output = test_data %>% merge(hotel_cluster_obs, by = key(hotel_cluster_obs), all.x = T)
-head(output)                                                                  
-submission = output %>% select(id, hotel_cluster = most_common) %>% 
-  mutate(hotel_cluster = replace(hotel_cluster, is.na(hotel_cluster), best_five)) %>% 
+output_leak_1 = test_data %>% merge(hotel_cluster_obs, by = key(hotel_cluster_obs), all.x = F)
+system.time({
+  hotel_cluster_obs = train_data_subset[ , list(most_common = most_common(hotel_cluster)), by = key(train_data_subset)] 
+})
+
+test_non_leak = test_data %>% subset(!(id %in% output_leak_1$id))
+
+# Using a smaller grouping variable excluding the orig_dest_distance to see if that gets us anywhere
+train_data_subset = train_data
+
+setkeyv(train_data_subset, c('user_location_country', 'user_location_region', 'user_location_city', 'hotel_market'))
+
+system.time({
+  hotel_cluster_obs = train_data_subset[ , list(most_common = most_common(hotel_cluster)), by = key(train_data_subset)] 
+})
+
+output_leak_2 = test_non_leak %>% merge(hotel_cluster_obs, by = key(hotel_cluster_obs), all.x = F)
+test_non_leak = test_data %>% subset(!(id %in% c(output_leak_1$id, output_leak_2$id)))
+
+test_non_leak$most_common = best_five
+
+submission = output_leak_1 %>% rbind(output_leak_2) %>% rbind(test_non_leak) %>% 
+  select(id, hotel_cluster = most_common) %>% 
+  # mutate(hotel_cluster = replace(hotel_cluster, is.na(hotel_cluster), best_five)) %>% 
   arrange(id)
 
-write.csv(submission, "../Data/kaggle/expedia/submissions/sub3.csv", row.names = F)
+z <- gzfile("../Data/kaggle/expedia/submissions/sub5.csv.gz")
+write.csv(submission, z, row.names = F)
 
                                                                   
