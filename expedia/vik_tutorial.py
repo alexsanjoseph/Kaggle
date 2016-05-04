d5536c2b915caacb6c9bf084f44933acca0576c9
alexsanjoseph <alexsanjoseph@gmail.com>
2016-05-04 21:14:10 +0530

    [expedia] vik tutorial

---
+++ expedia/vik_tutorial.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+destinations = pd.read_csv("../../Data/kaggle/expedia/destinations.csv")
+test = pd.read_csv("../../Data/kaggle/expedia/test.csv")
+train = pd.read_csv("../../Data/kaggle/expedia/train.csv")
